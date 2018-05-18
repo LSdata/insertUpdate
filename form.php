9d54d56c4a54dd2f775f0e95375da62699be414a
LSdata <LSdata@users.noreply.github.com>
2018-05-18 18:44:07 +0200

    init

---
+++ form.php
@@ -0,0 +1,8 @@
+<?php
+// fire off the request to /form.php
+        request = $.ajax({
+            url: "https://script.google.com/macros/s/AKfycbzV--xTooSkBLufMs4AnrCTdwZxVNtycTE4JNtaCze2UijXAg8/exec",
+            type: "post",
+            data: serializedData
+        });
+?>
