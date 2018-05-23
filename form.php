b48e749aac5e71cb5533fdad66c218e91596b0ad
LSdata <LSdata@users.noreply.github.com>
2018-05-23 14:40:45 +0200

    Delete form.php

--- form.php
+++
@@ -1,8 +0,0 @@
-<?php
-// fire off the request to /form.php
-        request = $.ajax({
-            url: "https://script.google.com/macros/s/AKfycbzV--xTooSkBLufMs4AnrCTdwZxVNtycTE4JNtaCze2UijXAg8/exec",
-            type: "post",
-            data: serializedData
-        });
-?>
